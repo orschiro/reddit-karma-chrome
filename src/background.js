0cb858cb8ce6d22a04d5e930a236e59530f4e2fb
Robert Orzanna <robert@orzanna.de>
2017-10-31 09:42:05 +0100

    Prepare xhr get json

--- src/background.js
+++ src/background.js
@@ -1,10 +1,13 @@
-// https://developer.chrome.com/extensions/xhr
 var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = handleStateChange; // Implemented elsewhere.
-xhr.open("GET", chrome.extension.getURL('https://www.reddit.com/api/me.json'), true);
+xhr.open("GET", "https://www.reddit.com/api/me.json", true);
+xhr.onreadystatechange = function() {
+  if (xhr.readyState == 4) {
+    // JSON.parse does not evaluate the attacker's scripts.
+    var karma = JSON.parse(xhr.responseText);
+  }
+}
 xhr.send();
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-  // Badge won't be able to disply more than three characters :(
-  chrome.browserAction.setBadgeText({text:"0"});
+  chrome.browserAction.setBadgeText({text:karma});
 });
