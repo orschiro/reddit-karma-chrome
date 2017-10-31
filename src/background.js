bda04b9efdaa9db1366ace0d6cad5dda8cd7a8f1
Robert Orzanna <robert@orzanna.de>
2017-10-31 16:47:45 +0100

    Moved browserAction inside ajax call

--- src/background.js
+++ src/background.js
@@ -4,10 +4,7 @@ xhr.onreadystatechange = function() {
   if (xhr.readyState == 4) {
     // JSON.parse does not evaluate the attacker's scripts.
     var karma = JSON.parse(xhr.responseText);
+    chrome.browserAction.setBadgeText({ text:karma.data.comment_karma });
   }
 }
 xhr.send();
-
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.browserAction.setBadgeText({text:karma});
-});
